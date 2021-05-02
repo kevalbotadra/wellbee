5c753a0c76b11f5486c451f8d2c1104681331d98
qoobes <qoobeethegreat@gmail.com>
2021-05-02 02:22:56 +0200

    Smoother experience

--- src/components/LoadingView.jsx
+++ src/components/LoadingView.jsx
@@ -1,18 +1,15 @@
 import { Box, CircularProgress } from "@material-ui/core";
-import DefaultLayout from "../layouts/DefaultLayout";
 
 export const LoadingView = () => (
-  <DefaultLayout inLoading>
-    <Box
-      width="100%"
-      height="60vh"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <CircularProgress color="primary" />
-    </Box>
-  </DefaultLayout>
+  <Box
+    width="100%"
+    height="90vh"
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+  >
+    <CircularProgress style={{ color: "#EBBC56" }} color="#EBBC56" />
+  </Box>
 );
 
 export const SimpleLoadingView = () => (
@@ -23,6 +20,6 @@ export const SimpleLoadingView = () => (
     justifyContent="center"
     alignItems="center"
   >
-    <CircularProgress color="primary" />
+    <CircularProgress style={{ color: "#EBBC56" }} color="#EBBC56" />
   </Box>
 );
